e869401c1a876b9a2088ff22b7096b0135930f0a
Loïc Messela <messelaloic@gmail.com>
2023-02-20 10:34:22 +0100

    Expense model created

---
+++ models/Expense.model.js
@@ -0,0 +1,21 @@
+const {Schema, model} = require('mongoose')
+
+const expenseSchema = new Schema({
+date: {
+    type: Date,
+    required: true 
+},
+price: {
+    type: Number,
+    required: true,
+},
+category: {type: String,
+    enum: ["Must have", "Nice to have", "Wasted"] 
+
+}
+tag: {
+    type: {mongoose.Schema.Types.ObjectId}, 
+    user: {type: mongoose.Schema.Types.ObjectId, 
+        ref: "User"}
+} 
+})
