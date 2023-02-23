10d6e1f218b408739a2b1fadc329508f443dcb6e
Loïc Messela <messelaloic@gmail.com>
2023-02-23 10:17:53 +0100

    style update

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -13,12 +13,11 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 
 router.get("/", async (req, res, next) => {
   const userFound = await User.findById(req.session.currentUser._id);
-  console.log("the currtnt user", userFound);
+  console.log("the current user", userFound);
   res.render("userProfile", { userFound });
 });
 
 router.post("/", async (req, res, next) => {
-  // const { id } = req.params;
   const userEdit = { ...req.body };
   try {
     await User.findByIdAndUpdate(req.session.currentUser._id, userEdit);
